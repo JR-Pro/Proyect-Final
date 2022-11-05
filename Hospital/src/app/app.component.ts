b7becd06f20553687a306ee049bf5a943d303355
JR-Pro <you@example.com>
2022-11-05 13:20:00 -0600

    hh

--- Hospital/src/app/app.component.ts
+++ Hospital/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ConsultaService } from './service/Consulta/consulta.service';
+import { EspecialidadService } from './service/Especialidad/especialidad.service';
+import { LaboratorioService } from './service/Laboratorio/laboratorio.service';
+import { SedesService } from './service/Sedes/sedes.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Hospital';
+
+  constructor(
+    public fb: FormBuilder,
+    public consultaService: ConsultaService,
+    public especialidadService: EspecialidadService,
+    public laboratorioService : LaboratorioService,
+    public sedesService : SedesService 
+
+  ){
+
+  }
+  ngOnInit(): void {
+    
+  }  
+  
 }
